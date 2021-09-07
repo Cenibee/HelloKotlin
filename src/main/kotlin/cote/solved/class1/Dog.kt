cd1d22e8cce43787335265dff3e88f0c0281dc30
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-09-07 17:50:58 +0900

    ALG: BOJ - 개

---
+++ src/main/kotlin/cote/solved/class1/Dog.kt
@@ -0,0 +1,9 @@
+package cote.solved.class1
+
+fun main() {
+    println("|\\_/|")
+    println("|q p|   /}")
+    println("( 0 )\"\"\"\\")
+    println("|\"^\"`    |")
+    println("||_/=\\\\__|")
+}
